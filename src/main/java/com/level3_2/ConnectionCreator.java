5a63c8f94d8bd7d04402e6289001d00bf2814f91
Inna Burlaka <inna_burlaka@ukr.net>
2023-08-27 17:38:37 +0300

    working project

--- src/main/java/com/level3_2/ConnectionCreator.java
+++ src/main/java/com/level3_2/ConnectionCreator.java
@@ -2,9 +2,6 @@ package com.level3_2;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 
 import static com.level3_2.dao.DAO.logger;
 
