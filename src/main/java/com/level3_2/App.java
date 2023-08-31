ebdfff042ad38f9463f299c36cc8b8b4db66f359
Inna Burlaka <inna_burlaka@ukr.net>
2023-08-31 14:39:25 +0300

    project connecting with Atlas

--- src/main/java/com/level3_2/App.java
+++ src/main/java/com/level3_2/App.java
@@ -36,10 +36,10 @@ public class App {
 //String uri = "mongodb://myMongoDb:admin124@docdb-2023-08-27-10-58-41." +
 //        "cgknngoatzj8.eu-central-1.docdb.amazonaws.com:27017/myMongoDb?readpreference=secondaryPreferred";
 //        String uri = "mongodb+srv://mongoInna:admin124@cluster0.qg9kdnn.mongodb.net/?retryWrites=true&w=majority";
-        String uri = properties.getProperty("uri");
+//       / String uri = properties.getProperty("uri");
 //        String uri = "mongodb://localhost:27017";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-//       / try (MongoClient mongoClient = new ConnectionCreator().createConnection()) {
+//        try (MongoClient mongoClient = MongoClients.create(uri)) {
+       try (MongoClient mongoClient = new ConnectionCreator().createConnection()) {
             logger.debug("MongoDB was created");
 
             MongoDatabase database = mongoClient.getDatabase("myMongoDb");
